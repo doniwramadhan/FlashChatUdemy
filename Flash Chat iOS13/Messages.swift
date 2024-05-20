2dd546291bddfd03185fb1fd9c7f45a17bd8abc5
Doni Wahyu Ramadhan <doniwramadhan@gmail.com>
2024-05-20 14:35:48 +0700

    first commit

---
+++ Flash Chat iOS13/Messages.swift
@@ -0,0 +1,14 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Doni on 5/17/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Messages {
+    let sender: String
+    let body: String
+}
